6a6e43c21bed110669342974468348c13d3e2558
AndresRicci93 <andresricci93@gmail.com>
2020-07-01 22:33:45 +0200

    commit

---
+++ warmup/minimax.js
@@ -0,0 +1,32 @@
+let arr = [7, 69, 2, 221, 8974];
+let sum = 0;
+let sum2 = 0;
+
+
+arr.sort(function (a, b) {
+  return a - b;
+});
+
+console.log(arr);
+function minmax(arr) {
+
+  arr.sort(function (a, b) {
+    return a - b;
+  });
+
+  for (let i = 1; i <= arr.length - 1; i++) {
+    sum += arr[i];
+  }
+
+  let minsum = arr.pop();
+
+ 
+  for (let i = 0; i <= arr.length - 1; i++) {
+    sum2 += arr[i];
+  }
+
+  console.log(sum2 + ' ' + sum);
+
+}
+
+console.log(minmax(arr));
