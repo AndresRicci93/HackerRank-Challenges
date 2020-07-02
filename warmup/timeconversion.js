67056aea010c5f3c2b94fa060488313fa716add6
AndresRicci93 <andresricci93@gmail.com>
2020-07-02 14:46:18 +0200

    commit

--- warmup/timeconversion.js
+++ warmup/timeconversion.js
@@ -5,15 +5,18 @@ let time = "07:05:45PM";
 function timeConversion(s) {
 
     //Split hour, minutes and seconds
+
     let hour = time.split(':')[0];
     let min = time.split(':')[1];
     let sec = time.split(':')[2].substring(0,2);
     let hourOfDay = time.split(':')[2].substring(2,4);
 
     // Noon is 12:00:00PM on a 12-hour clock, and 12:00:00 on a 24-hour clock
+
     if (hourOfDay === 'PM' && hour < 12) hour = parseInt(hour) + 12;
 
     // Midnight is 12:00:00AM on a 12-hour clock, and 00:00:00 on a 24-hour clock
+    
     if (hourOfDay === 'AM' && hour === '12') hour = '00';
 
     return hour + ':' + min + ':' + sec;
