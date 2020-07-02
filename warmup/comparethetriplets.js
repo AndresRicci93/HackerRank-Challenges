46317d31c31a5884a53637179fbc693d84ed87be
AndresRicci93 <andresricci93@gmail.com>
2020-07-02 14:32:34 +0200

    commit

--- warmup/comparethetriplets.js
+++ warmup/comparethetriplets.js
@@ -1,38 +1,31 @@
 let a = [5,6,7];
-let b = [3,6,10];
+let b = [3,7,10];
 let Alice = 0;
 let Bob = 0;
-function comparethetriplets(a,b) {
+let result = [];
+function compareTriplets(a, b) {
 
-for (let i = 0; i <= a.length; i++) {
+    for (let i = 0; i <= a.length; i++) {
+    
+        
+        if (a[i] > b[i]) {
+            Alice++;
+        }
+    
+        else if (a[i] < b[i]) {
+            Bob++;
+        }
+     }
 
-    //If a[i] > b[i], then Alice is awarded 1 point.
-    if (a[i] > b[i]) Alice++;
-    //If a[i] , then Bob is awarded 1 point.
-    if (a[i] < b[i]) Bob++;
-    //else neither Bob or Alice get a point
- }
-
-return Alice + ' ' + Bob;
-}
+     result[0] = Alice;
+     result[1] = " ";
+     result[2] = Bob;
+     
+     
+     return result;
+    
+    }
 
 
 console.log(comparethetriplets(a,b));
 
-/** If you get an error
- * 
- * Replace this:
- * 
- * const result = compareTriplets(a, b);
-
-    ws.write(result.join('') + '\n');
-
- * TypeError: result.join is not a function
- * 
- * with this:
- * let result = compareTriplets(a, b);
-
-    ws.write(result += "\n");;
- * 
- * 
- * **/
