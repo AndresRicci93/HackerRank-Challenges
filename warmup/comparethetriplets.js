c35bfa299499a3bc5fecd8dd70c1adc4a9a14b99
AndresRicci93 <andresricci93@gmail.com>
2020-07-02 12:45:35 +0200

    comparetriplets

--- warmup/comparethetriplets.js
+++ warmup/comparethetriplets.js
@@ -6,9 +6,11 @@ function comparethetriplets(a,b) {
 
 for (let i = 0; i <= a.length; i++) {
 
-    
+    //If a[i] > b[i], then Alice is awarded 1 point.
     if (a[i] > b[i]) Alice++;
+    //If a[i] , then Bob is awarded 1 point.
     if (a[i] < b[i]) Bob++;
+    //else neither Bob or Alice get a point
  }
 
 return Alice + ' ' + Bob;
@@ -16,3 +18,21 @@ return Alice + ' ' + Bob;
 
 
 console.log(comparethetriplets(a,b));
+
+/** If you get an error
+ * 
+ * Replace this:
+ * 
+ * const result = compareTriplets(a, b);
+
+    ws.write(result.join('') + '\n');
+
+ * TypeError: result.join is not a function
+ * 
+ * with this:
+ * let result = compareTriplets(a, b);
+
+    ws.write(result += "\n");;
+ * 
+ * 
+ * **/
