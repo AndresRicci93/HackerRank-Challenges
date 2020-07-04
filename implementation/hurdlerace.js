c8369df32cb1893e6da832e3f4b8852ddf7b75ab
AndresRicci93 <andresricci93@gmail.com>
2020-07-04 19:49:16 +0200

    hurdlerace

--- implementation/hurdlerace.js
+++ implementation/hurdlerace.js
@@ -1,12 +1,8 @@
 
 let height = [1,6,3,5,2];
 let k = 4;
-function hurdleRace(k, height) {
 
-
-    return Math.max(...height) > k ? Math.max(...height) - k : 0 ;
-    
-    }
+const hurdleRace = (k, height) => Math.max(...height) > k ? Math.max(...height) - k : 0;
 
 
     console.log(hurdleRace(k,height));
