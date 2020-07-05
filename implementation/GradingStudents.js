609b94408effe80768f9d4e0c9c1d03234024932
AndresRicci93 <andresricci93@gmail.com>
2020-07-05 12:35:27 +0200

    ref

--- implementation/GradingStudents.js
+++ implementation/GradingStudents.js
@@ -1,11 +1,8 @@
 let grades = [73,67,38,33];
 
 
+const gradingStudents = (grades) => grades.map(grade => (Math.ceil(grade/5)*5 - grade >= 3 || grade <38) ? grade : Math.ceil(grade/5)*5);
 
 
-function gradingStudents(grades) {
-    return grades.map(grade => (Math.ceil(grade/5)*5 - grade >= 3 || grade <38) ? grade : Math.ceil(grade/5)*5);
-
-}
 
 console.log(gradingStudents(grades));
